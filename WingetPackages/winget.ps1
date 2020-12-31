ea83c28e54f45498cb996d2e7abb2aca5dc51075
Mark Purdon <mark@hewmann.com>
2020-12-31 19:38:27 +0000

    Added winget packages

---
+++ WingetPackages/winget.ps1
@@ -0,0 +1,14 @@
+winget install --id="WhatsApp.WhatsApp" --exact --silent ;
+winget install --id="Git.Git" --exact --silent ;
+winget install --id="Microsoft.dotnet" --exact --silent ; 
+winget install --id="LogMeIn.LastPass" --exact --silent ; 
+winget install --id="SlackTechnologies.Slack" --exact --silent ; 
+winget install --id="Postman.Postman" --exact --silent ; 
+winget install --id="Microsoft.Teams" --exact --silent ; 
+winget install --id="Microsoft.VisualStudioCode" --exact --silent ; 
+winget install --id="Microsoft.AzureDataStudio" --exact --silent ;
+winget install --id="Signal.Signal" --exact --silent; 
+winget install --id="Canonical.Ubuntu" --exact --silent; 
+winget install --id="Microsoft.VisualStudioCodeInsiders" --exact --silent ; 
+winget install --id="Microsoft.dotnetPreview" --exact --silent ; 
+winget install --id="Docker.DockerDesktopEdge" --exact --silent 
